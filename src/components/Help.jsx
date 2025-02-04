a3b0af21316c3f6e4290890f5d1deb2d8f969da7
RamenBoy0 <isaactanyiwei@gmail.com>
2025-02-04 18:30:58 +0800

    Update Help.jsx

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -3,5 +3,9 @@ import React from 'react'
 
 export default function HelpSupport() {
 
-    return <div>Help & Support</div>
+    return     <div>
+    <p>
+        The main purpose of the FM Pulse Cost Calculator is to establish a cost benchmark for users.
+    </p>
+</div>
 }
